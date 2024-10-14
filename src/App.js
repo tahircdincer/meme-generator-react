f746b04fbd920bfa0ee3aaf5258b688f8d523450
Tahir Dincer <coskun.dincer2014@gmail.com>
2024-10-14 03:36:02 +0300

    '
    Form added
    
    ' Functionality ' ' implemented ' to the button so button can get ' memes ' from ' memesData ' '
    
    ' styling ' implemented ' to the ' app '
    
    '

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import Navbar from "./components/Navbar";
+import Form from "./components/Form";
+import memesData from "./data/memesData";
 
 export default function App() {
-  return <Navbar />;
+  return (
+    <div className="app">
+      <Navbar />
+      <div className="app__form--container">
+        <Form />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,27 @@
+import React from "react";
+import memesData from "../data/memesData";
+
+export default function Form() {
+  const [memeImage, setMemeImage] = React.useState("");
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
+
+  return (
+    <div>
+      <div className="form">
+        <div className="form__inpt--area">
+          <input type="text" placeholder="Enter top text" />
+          <input type="text" placeholder="Enter bottom text" />
+        </div>
+        <button className="form__button" onClick={getMemeImage}>
+          Get Meme 🖼️
+        </button>
+      </div>
+      <img src={memeImage} alt=" migm " />
+    </div>
+  );
+}
